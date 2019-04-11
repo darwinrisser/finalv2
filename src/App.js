9818031ba9eb68f596859515ec177adb35d72c7b
Darwin Risser <riser.darwin@gmail.com>
2019-04-11 09:31:27 -0500

    working on search

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
 import './App.css';
+import AppBar from './components/AppBar';
 import SignIn from './components/SignIn';
 import SignUp from './components/SignUp';
 import UserList from './components/UserList';
+
 
 class App extends Component {
   
@@ -20,13 +22,22 @@ class App extends Component {
   onChange = e => {
     this.setState({ search : e.target.value });
   }
-  
+
   render() {
+    const {search} = this.state;
+    const filteredUsers = [];
+    filteredUsers = this.state.users.filter(user=>{
+      return user.instrument.toLowerCase().indexOf( search.toLowerCase() ) !== -1
+    })
+
+    
     return (
       <div className="App">
+        <AppBar/>
+        <input type="text" label="narrow down your search" placeholder="Search" onChange={this.onchange} search={this.state.search}/>
         {/* <SignIn/> */}
         {/* <SignUp/> */}
-        <UserList users={this.state.users} onChange={this.onChange} search={this.state.search}/>
+        <UserList users={this.filteredUsers}/>
       </div>
     );
   }
